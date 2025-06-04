6290922e084fcc2c95b5e8445541397583e72f43
Asif-Iqbal-Gazi <Asif-Iqbal-Gazi@users.noreply.github.com>
2025-06-03 22:06:01 -0700

    Add support for interface argument with fallback
    - Accepts network interface as an argument.
    - If none is provided, it shows available interfaces.
    - Defaults to the first available interface.
    - Informs the user of selected/defaulted interface.

--- main.c
+++ main.c
@@ -1,26 +1,54 @@
 #include <pcap.h>
-#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 #include <sys/types.h>
 
-int main(void) {
+int main(int argc, char **argv) {
   char errbuf[PCAP_ERRBUF_SIZE];
-  pcap_if_t *alldevs, *d;
+  pcap_if_t *alldevs, *device;
+  char *selected_if = NULL;
 
   if (pcap_findalldevs(&alldevs, errbuf) == -1) {
     fprintf(stderr, "Error finding devices: %s\n", errbuf);
     return 1;
   }
 
-  printf("Available devices:\n");
-  printf("+----------------------------------------+\n");
-
-  for (d = alldevs; d != NULL; d = d->next) {
-    printf("%s\n", d->name);
+  if (alldevs == NULL) {
+    fprintf(stderr, "No network interfaces found.\n");
+    return 1;
   }
 
+  if (argc > 1) {
+    for (device = alldevs; device != NULL; device = device->next) {
+      if (strcmp(device->name, argv[1]) == 0) {
+        selected_if = device->name;
+        break;
+      }
+    }
+
+    if (!selected_if) {
+      fprintf(stderr, "Provided interface '%s' not found. Exiting.\n", argv[1]);
+      pcap_freealldevs(alldevs);
+      return 1;
+    }
+  } else {
+    selected_if = alldevs->name;
+
+    printf("\n+----------------------------------------+\n");
+    printf("| No interface provided.                 |\n");
+    printf("| Available interfaces:                  |\n");
+    printf("+----------------------------------------+\n");
+    for (device = alldevs; device != NULL; device = device->next) {
+      printf("- %s\n", device->name);
+    }
+    printf("+----------------------------------------+\n");
+    printf("Defaulting to: %s\n", selected_if);
+  }
+
+  printf("\nUsing interface: %s\n", selected_if);
   printf("+----------------------------------------+\n");
+
   pcap_freealldevs(alldevs);
   return 0;
 }
